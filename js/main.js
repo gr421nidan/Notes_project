3da79d49c5f01d72bbbf3aeadef8bac66eced8e9
gr421nidan <nikushkina.dasha@yandex.ru>
2024-01-31 22:15:51 +0700

    Добавление метода для перехода во второй стобец и его ограничения

--- js/main.js
+++ js/main.js
@@ -17,4 +17,24 @@ let app = new Vue({
             }
 
         },
+        scoreRadio() {
+            if(this.notesListDo.length>=5){
+                alert('Нельзя добавить')
+                return
+            }
+            else{
+                this.score += 1
+                console.log(this.score)
+                if (this.score >=3 && this.score < 5){
+                    for(let element in this.notesList) {
+                        this.notesList.splice(this.notesList[element], 1)
+                    }
+                    this.notesListDo.push(this.note)
+                    this.score = 0
+                }
+            }
+
+
+        },
+
     }})
